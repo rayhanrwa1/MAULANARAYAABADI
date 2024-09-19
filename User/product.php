4100d695774efabe83423e857e37c6c9521e30e0
rayhanrwa1 <kulm2223@gmail.com>
2024-09-20 06:49:18 +0700

    Deploy Versi 5.0.4 20/09/2024

--- User/product.php
+++ User/product.php
@@ -240,7 +240,7 @@ function formatRupiah($number) {
                                             // Use the product_photo_update field to get the relative image path
                                         $relativePathFromSQL = $row['product_photo_update']; // Example: 'assets/image_db/product.jpg'
                                         // Correct the path by prepending "../admin/"
-                                        $imagePath = "../admin/assets/image_db/produk/" . $relativePathFromSQL;
+                                        $imagePath = "/admin/assets/image_db/produk/" . $relativePathFromSQL;
                                     ?>
                                             <div class="col-xl-3 col-lg-4 col-sm-6 col-6">
                                                 <div class="ltn__product-item ltn__product-item-3 text-center">
